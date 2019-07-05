cf62bc9658c7ed51c2fe0e8a115c2c48c8969045
Florian Wilhelm <florian.wilhelm02@sap.com>
2019-07-05 13:39:00 +0200

    update

--- runTests.sh
+++ runTests.sh
@@ -21,12 +21,14 @@ docker tag localhost:5000/ppiper/cf-cli:latest ppiper/cf-cli:latest
 docker push localhost:5000/ppiper/cf-cli:latest
 
 git clone https://github.com/piper-validation/cloud-s4-sdk-book.git -b validate-cf-cli test-project
-cd test-project
+pushd test-project
 
 docker run -v //var/run/docker.sock:/var/run/docker.sock -v $(pwd):/workspace -v /tmp \
  -e CX_INFRA_IT_CF_PASSWORD -e CX_INFRA_IT_CF_USERNAME -e BRANCH_NAME=master \
  -e CASC_JENKINS_CONFIG=/workspace/jenkins.yml -e HOST=$(hostname) \
  ppiper/jenkinsfile-runner
+
+popd
 
 function cleanup {
   rm -rf test-project
